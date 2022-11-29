340e70d8a8c2fbd3744076f325a86e5f6c3b7ac9
Jia Jun <trelorjun98@gmail.com>
2022-11-29 21:44:49 +0800

    add_batch_windows_program

---
+++ copy.bat
@@ -0,0 +1,3 @@
+echo D | xcopy /s/e "C:\Users\jiaju\Desktop\LiannFYP\qap" %1
+
+echo We Have Finally Done
---
+++ restart.bat
@@ -0,0 +1,6 @@
+cd C:\xampp\ 
+start xampp_stop.exe
+start xampp_start.exe
+
+cd "C:\Program Files\Google\Chrome\Application"
+start chrome.exe http://%1
